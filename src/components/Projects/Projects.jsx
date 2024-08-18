371cb0d0934248a7c86689b48a58aa70adc0a2ad
NVadim <palaginvad@gmail.com>
2024-08-18 14:13:13 +0300

    sync

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
-import { Box, Typography, Grid } from '@mui/material';
+import { Box, Typography, Grid, ButtonBase } from '@mui/material';
 import { useTheme, useMediaQuery } from '@mui/material';
+import Icons from '../Icons/Icons.jsx';
+import tapImg from '../../img/taptap.png';
+import tapMobile from '../../img/tapMobile.png';
+import tomoImg from '../../img/tomo.png';
+import tomoMob from '../../img/tomoMob.png';
+import benImg from '../../img/benDog.png';
+import benMob from '../../img/benMob.png';
 
 const Projects = () => {
 	const theme = useTheme();
 	const isMobile = useMediaQuery(theme.breakpoints.down('1280'));
+
+	const iconButtonStyles = {
+		width: 66,
+		height: 66,
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#0a0e35',
+		padding: 0,
+		transition: '.25s',
+		borderRadius: '50%',
+		'&:hover': {
+			backgroundColor: 'rgba(10, 14, 53, 0.8)',
+			transition: '.25s',
+		},
+	};
+
 	return (
-		<Box sx={{
-            display: 'flex',
-            flexDirection: 'column'
-        }}>
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+			}}
+		>
 			<Typography
 				variant='h2'
 				sx={{
@@ -36,256 +62,311 @@ const Projects = () => {
 					marginTop: '24px',
 				}}
 			>
-                {isMobile ? (
-						<>
-							Discover the various <br /> projects we’ve developed. <br /> Take a look and see the kind <br/>
-				of innovative solutions we <br/> can create.
-						</>
-					) : (
-            <>
-           Discover the various projects we’ve developed. Take a look and <br /> see the kind
-				of innovative solutions we can create.
-            </>
-					)}
+				{isMobile ? (
+					<>
+						Discover the various <br /> projects we’ve developed. <br /> Take a look and
+						see the kind <br />
+						of innovative solutions we <br /> can create.
+					</>
+				) : (
+					<>
+						Discover the various projects we’ve developed. Take a look and <br /> see the
+						kind of innovative solutions we can create.
+					</>
+				)}
 			</Typography>
 
-            <Grid container spacing={4} 
-            sx={{ marginTop: isMobile ? '30px' : '70px' }}>
-        <Grid item xs={12}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '200px',
-              backgroundColor: '#e0f7fa',
-              borderRadius: '8px',
-              padding: '16px',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-            }}
-          >
-            <Typography
-              variant="h6"
-              sx={{
-                fontWeight: 700,
-                fontSize: '24px',
-                color: '#000',
-                marginBottom: '8px',
-              }}
-            >
-              Project One
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{
-                fontWeight: 400,
-                fontSize: '16px',
-                lineHeight: '160%',
-                color: '#333',
-                textAlign: 'center',
-              }}
-            >
-              Brief description of project one.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '200px',
-              backgroundColor: '#ffebee',
-              borderRadius: '8px',
-              padding: '16px',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-            }}
-          >
-            <Typography
-              variant="h6"
-              sx={{
-                fontWeight: 700,
-                fontSize: '24px',
-                color: '#000',
-                marginBottom: '8px',
-              }}
-            >
-              Project Two
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{
-                fontWeight: 400,
-                fontSize: '16px',
-                lineHeight: '160%',
-                color: '#333',
-                textAlign: 'center',
-              }}
-            >
-              Brief description of project two.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '200px',
-              backgroundColor: '#fff9c4',
-              borderRadius: '8px',
-              padding: '16px',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-            }}
-          >
-            <Typography
-              variant="h6"
-              sx={{
-                fontWeight: 700,
-                fontSize: '24px',
-                color: '#000',
-                marginBottom: '8px',
-              }}
-            >
-              Project Three
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{
-                fontWeight: 400,
-                fontSize: '16px',
-                lineHeight: '160%',
-                color: '#333',
-                textAlign: 'center',
-              }}
-            >
-              Brief description of project three.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '200px',
-              backgroundColor: '#e1bee7',
-              borderRadius: '8px',
-              padding: '16px',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-            }}
-          >
-            <Typography
-              variant="h6"
-              sx={{
-                fontWeight: 700,
-                fontSize: '24px',
-                color: '#000',
-                marginBottom: '8px',
-              }}
-            >
-              Project Four
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{
-                fontWeight: 400,
-                fontSize: '16px',
-                lineHeight: '160%',
-                color: '#333',
-                textAlign: 'center',
-              }}
-            >
-              Brief description of project four.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '200px',
-              backgroundColor: '#c5e1a5',
-              borderRadius: '8px',
-              padding: '16px',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-            }}
-          >
-            <Typography
-              variant="h6"
-              sx={{
-                fontWeight: 700,
-                fontSize: '24px',
-                color: '#000',
-                marginBottom: '8px',
-              }}
-            >
-              Project Five
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{
-                fontWeight: 400,
-                fontSize: '16px',
-                lineHeight: '160%',
-                color: '#333',
-                textAlign: 'center',
-              }}
-            >
-              Brief description of project five.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '200px',
-              backgroundColor: '#ffab91',
-              borderRadius: '8px',
-              padding: '16px',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-            }}
-          >
-            <Typography
-              variant="h6"
-              sx={{
-                fontWeight: 700,
-                fontSize: '24px',
-                color: '#000',
-                marginBottom: '8px',
-              }}
-            >
-              Project Six
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{
-                fontWeight: 400,
-                fontSize: '16px',
-                lineHeight: '160%',
-                color: '#333',
-                textAlign: 'center',
-              }}
-            >
-              Brief description of project six.
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
+			<Grid container sx={{ marginTop: isMobile ? '30px' : '70px' }}>
+				<Grid item xs={12} style={{ display: 'flex', justifyContent: 'center' }}>
+					<Box
+						sx={{
+							background: 'linear-gradient(91deg, #c7acff 0%, #fff 87.06%)',
+							borderRadius: '24px 24px 0 24px',
+							boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+							width: isMobile ? '100%' : '1340px',
+							display: 'flex',
+							flexDirection: isMobile ? 'column' : 'row',
+							padding: isMobile ? '24px' : '50px 50px 50px 90px',
+							gap: isMobile ? '22px' : '50px',
+							alignItems: isMobile ? 'center' : 'flex-start',
+						}}
+					>
+						<Box sx={{ flex: 1, textAlign: isMobile ? 'center' : 'left' }}>
+							<Typography variant='h2'>Tap Swap</Typography>
+							<Typography variant='body1' style={{ marginTop: '16px' }}>
+								TapSwap is a cutting-edge financial platform where users can earn tokens
+								by utilizing the app's various mining features. Your main goal is to
+								maximize your in-game currency, Shares, by completing missions, inviting
+								friends, and using different app boosts. Shares can later be exchanged for
+								TAP tokens. 
+							</Typography>
+							{!isMobile && (
+								<Box sx={{ display: 'flex', gap: 2, marginTop: '28px' }}>
+									<ButtonBase
+										component='a'
+										href='https://www.facebook.com'
+										target='_blank'
+										color='primary'
+										sx={iconButtonStyles}
+									>
+										<Icons.Facebook />
+									</ButtonBase>
+									<ButtonBase
+										component='a'
+										href='https://t.me'
+										target='_blank'
+										color='primary'
+										sx={iconButtonStyles}
+									>
+										<Icons.TG />
+									</ButtonBase>
+									<ButtonBase
+										component='a'
+										href='https://discord.com'
+										target='_blank'
+										color='primary'
+										sx={iconButtonStyles}
+									>
+										<Icons.Discord />
+									</ButtonBase>
+								</Box>
+							)}
+						</Box>
+						<Box sx={{ flex: 1, textAlign: 'center' }}>
+							<img
+								src={isMobile ? tapMobile : tapImg}
+								alt='Tap Swap'
+								style={{ width: '100%', borderRadius: '16px' }}
+							/>
+						</Box>
+						{isMobile && (
+							<Box sx={{ display: 'flex', gap: 2, marginTop: '24px' }}>
+								<ButtonBase
+									component='a'
+									href='https://www.facebook.com'
+									target='_blank'
+									color='primary'
+									sx={iconButtonStyles}
+								>
+									<Icons.Facebook />
+								</ButtonBase>
+								<ButtonBase
+									component='a'
+									href='https://t.me'
+									target='_blank'
+									color='primary'
+									sx={iconButtonStyles}
+								>
+									<Icons.TG />
+								</ButtonBase>
+								<ButtonBase
+									component='a'
+									href='https://discord.com'
+									target='_blank'
+									color='primary'
+									sx={iconButtonStyles}
+								>
+									<Icons.Discord />
+								</ButtonBase>
+							</Box>
+						)}
+					</Box>
+				</Grid>
+			</Grid>
+			<Grid container sx={{ marginTop: '30px' }}>
+				<Grid item xs={12} style={{ display: 'flex', justifyContent: 'center' }}>
+					<Box
+						sx={{
+							background: 'linear-gradient(91deg, #c7acff 0%, #fff 87.06%)',
+							borderRadius: '24px 24px 0 24px',
+							boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+							width: isMobile ? '100%' : '1340px',
+							display: 'flex',
+							flexDirection: isMobile ? 'column' : 'row',
+							padding: isMobile ? '24px' : '50px 50px 50px 90px',
+							gap: isMobile ? '22px' : '50px',
+							alignItems: isMobile ? 'center' : 'flex-start',
+						}}
+					>
+						<Box sx={{ flex: 1, textAlign: isMobile ? 'center' : 'left' }}>
+							<Typography variant='h2'>TomoCat</Typography>
+							<Typography variant='body1' style={{ marginTop: '16px' }}>
+								Let me introduce myself — I’m Tomo, a stray cat. Tired and sad, I gain
+								magical powers when you take care of me. just pet me and I’ll bring your
+								rewards. To boost them, think about my food, style, and fun. Also,
+								surround me with your friends. A meow fam purrs with more bonuses
+							</Typography>
+							{!isMobile && (
+								<Box sx={{ display: 'flex', gap: 2, marginTop: '28px' }}>
+									<ButtonBase
+										component='a'
+										href='https://www.facebook.com'
+										target='_blank'
+										color='primary'
+										sx={iconButtonStyles}
+									>
+										<Icons.Facebook />
+									</ButtonBase>
+									<ButtonBase
+										component='a'
+										href='https://t.me'
+										target='_blank'
+										color='primary'
+										sx={iconButtonStyles}
+									>
+										<Icons.TG />
+									</ButtonBase>
+									<ButtonBase
+										component='a'
+										href='https://discord.com'
+										target='_blank'
+										color='primary'
+										sx={iconButtonStyles}
+									>
+										<Icons.Discord />
+									</ButtonBase>
+								</Box>
+							)}
+						</Box>
+						<Box sx={{ flex: 1, textAlign: 'center' }}>
+							<img
+								src={isMobile ? tomoMob : tomoImg}
+								alt='Tap Swap'
+								style={{ width: '100%', borderRadius: '16px' }}
+							/>
+						</Box>
+						{isMobile && (
+							<Box sx={{ display: 'flex', gap: 2, marginTop: '24px' }}>
+								<ButtonBase
+									component='a'
+									href='https://www.facebook.com'
+									target='_blank'
+									color='primary'
+									sx={iconButtonStyles}
+								>
+									<Icons.Facebook />
+								</ButtonBase>
+								<ButtonBase
+									component='a'
+									href='https://t.me'
+									target='_blank'
+									color='primary'
+									sx={iconButtonStyles}
+								>
+									<Icons.TG />
+								</ButtonBase>
+								<ButtonBase
+									component='a'
+									href='https://discord.com'
+									target='_blank'
+									color='primary'
+									sx={iconButtonStyles}
+								>
+									<Icons.Discord />
+								</ButtonBase>
+							</Box>
+						)}
+					</Box>
+				</Grid>
+			</Grid>
+			<Grid container sx={{ marginTop: '30px' }}>
+				<Grid item xs={12} style={{ display: 'flex', justifyContent: 'center' }}>
+					<Box
+						sx={{
+							background: 'linear-gradient(320deg, #6462f1 27.09%, #aaa9ff 100%)',
+							borderRadius: '24px 24px 0 24px',
+							boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+							width: isMobile ? '100%' : '1340px',
+							display: 'flex',
+							flexDirection: isMobile ? 'column' : 'row',
+							padding: isMobile ? '24px' : '50px 50px 50px 90px',
+							gap: isMobile ? '22px' : '50px',
+							alignItems: isMobile ? 'center' : 'flex-start',
+						}}
+					>
+						<Box sx={{ flex: 1, textAlign: isMobile ? 'center' : 'left' }}>
+							<Typography variant='h2'>BenDog</Typography>
+							<Typography variant='body1' style={{ marginTop: '16px' }}>
+								I'm Ben, a successful dog.
+							</Typography>
+							<Typography variant='body1' style={{ marginTop: '16px' }}>
+								Feeling tired and lonely, I discover my magical powers when you take care
+								of me. Give me some love and I will bring you a reward. To get even more
+								bonuses, improve my abilities.
+							</Typography>
+							{!isMobile && (
+								<Box sx={{ display: 'flex', gap: 2, marginTop: '28px' }}>
+									<ButtonBase
+										component='a'
+										href='https://www.facebook.com'
+										target='_blank'
+										color='primary'
+										sx={iconButtonStyles}
+									>
+										<Icons.Facebook />
+									</ButtonBase>
+									<ButtonBase
+										component='a'
+										href='https://t.me'
+										target='_blank'
+										color='primary'
+										sx={iconButtonStyles}
+									>
+										<Icons.TG />
+									</ButtonBase>
+									<ButtonBase
+										component='a'
+										href='https://discord.com'
+										target='_blank'
+										color='primary'
+										sx={iconButtonStyles}
+									>
+										<Icons.Discord />
+									</ButtonBase>
+								</Box>
+							)}
+						</Box>
+						<Box sx={{ flex: 1, textAlign: 'center' }}>
+							<img
+								src={isMobile ? benMob : benImg}
+								alt='Tap Swap'
+								style={{ width: '100%', borderRadius: '16px' }}
+							/>
+						</Box>
+						{isMobile && (
+							<Box sx={{ display: 'flex', gap: 2, marginTop: '24px' }}>
+								<ButtonBase
+									component='a'
+									href='https://www.facebook.com'
+									target='_blank'
+									color='primary'
+									sx={iconButtonStyles}
+								>
+									<Icons.Facebook />
+								</ButtonBase>
+								<ButtonBase
+									component='a'
+									href='https://t.me'
+									target='_blank'
+									color='primary'
+									sx={iconButtonStyles}
+								>
+									<Icons.TG />
+								</ButtonBase>
+								<ButtonBase
+									component='a'
+									href='https://discord.com'
+									target='_blank'
+									color='primary'
+									sx={iconButtonStyles}
+								>
+									<Icons.Discord />
+								</ButtonBase>
+							</Box>
+						)}
+					</Box>
+				</Grid>
+			</Grid>
 		</Box>
 	);
 };
